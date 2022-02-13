5a05e410e387f3ddf4dc607dbfd8d5bc0ffb57da
Cyber108705 <98073055+Cyber108705@users.noreply.github.com>
2022-02-13 07:39:42 +0000

    change js file

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var api = "25631434-859bf3166bb26e033af830823";
 var form = document.getElementById("form");
-form.addEventListener("submit",formSubmit=(e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-})
-
-formSubmit=(e)=>{
     let search = document.getElementById("search");
-    let values = ''
-    if(search==null){
-    	values='letest'
-    }else{
-    	values = search.value;
+    let values = "";
+    if (search == null) {
+        values = "letest";
+    } else {
+        values = search.value;
     }
-    var URL = `https://pixabay.com/api/?key=${api}&q=${encodeURIComponent(values)}&per_page=40&pretty=true`;
+    var URL = `https://pixabay.com/api/?key=${api}&q=${encodeURIComponent(
+    values
+  )}&per_page=40&pretty=true`;
     fetch(URL)
         .then((data) => {
             return data.json();
@@ -20,7 +19,7 @@ formSubmit=(e)=>{
         .then((res) => {
             let hits = res.hits;
             document.getElementById("root").innerHTML = null;
-            console.log(hits)
+            console.log(hits);
             for (let x in hits) {
                 let img = hits[x];
                 let box = document.createElement("div");
@@ -37,7 +36,59 @@ formSubmit=(e)=>{
                 for (let i = 0; i < img.length; i++) {
                     if (viewPort(img[i])) {
                         img[i].setAttribute("src", img[i].getAttribute("data-src"));
-                        
+                    }
+                }
+            }
+
+            function viewPort(el) {
+                let rect = el.getBoundingClientRect();
+                return (
+                    rect.top >= 0 &&
+                    rect.left >= 0 &&
+                    rect.bottom <=
+                    (window.innerHeight || document.documentElement.clientHeight) &&
+                    rect.right <=
+                    (window.innerWidth || document.documentElement.clientWidth)
+                );
+            }
+        });
+});
+
+formSubmit = (e) => {
+    let search = document.getElementById("search");
+    let values = "";
+    if (search == null) {
+        values = "letest";
+    } else {
+        values = search.value;
+    }
+    var URL = `https://pixabay.com/api/?key=${api}&q=${encodeURIComponent(
+    values
+  )}&per_page=40&pretty=true`;
+    fetch(URL)
+        .then((data) => {
+            return data.json();
+        })
+        .then((res) => {
+            let hits = res.hits;
+            document.getElementById("root").innerHTML = null;
+            console.log(hits);
+            for (let x in hits) {
+                let img = hits[x];
+                let box = document.createElement("div");
+                box.classList.add("card");
+                box.innerHTML = `<img class="img lazyimg" data-src="${img.webformatURL}" alt="" src="./lazyimg.gif"><div class="side"><i class="fa fa-heart"></i><i class="fa fa-plus"></i></div><div class="down"><div class="profile"><img src="${img.userImageURL}" alt=""><b>${img.user}</b></div><i class="fa fa-download"></i></div>`;
+                document.getElementById("root").appendChild(box);
+            }
+            window.onscroll = function(ev) {
+                lazyload();
+            };
+
+            function lazyload() {
+                let img = document.querySelectorAll(".lazyimg");
+                for (let i = 0; i < img.length; i++) {
+                    if (viewPort(img[i])) {
+                        img[i].setAttribute("src", img[i].getAttribute("data-src"));
                     }
                 }
             }
@@ -56,7 +107,7 @@ formSubmit=(e)=>{
         });
 };
 
-formSubmit(this)
+formSubmit(this);
 // fhoem
 var api = "25631434-859bf3166bb26e033af830823";
 var form = document.getElementById("form_home");
@@ -89,7 +140,6 @@ form.addEventListener("submit", (e) => {
                 for (let i = 0; i < img.length; i++) {
                     if (viewPort(img[i])) {
                         img[i].setAttribute("src", img[i].getAttribute("data-src"));
-                       
                     }
                 }
             }
@@ -108,15 +158,11 @@ form.addEventListener("submit", (e) => {
         });
 });
 
-
 // menu
 
-document.getElementById('bars').addEventListener('click',()=>{
-	document.getElementById('menu').classList.toggle('container-show')
-})
-document.getElementById('cUs').addEventListener('click',()=>{
-	document.getElementById('contact').classList.toggle('contact-show')
-})
-
-
-
+document.getElementById("bars").addEventListener("click", () => {
+    document.getElementById("menu").classList.toggle("container-show");
+});
+document.getElementById("cUs").addEventListener("click", () => {
+    document.getElementById("contact").classList.toggle("contact-show");
+});
