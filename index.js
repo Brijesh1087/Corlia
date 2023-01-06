bfd6910a3a1afe57dcb860c9a322ae379217a9b4
brijeshpydev <brijeshpy@duck.com>
2023-01-06 22:48:45 +0530

    edit al files

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const get_images = (images) => {
 
     for(let x in images.hits){
         let card = document.createElement("img");
-    card.setAttribute('class','img-fluid mx-auto text-center rounded p-2 m-2 shadow-sm bg-white');
+    card.setAttribute('class','img-fluid mx-auto text-center rounded p-2 m-2 shadow-sm bg-white lazyimg');
     card.src=`${images.hits[x].webformatURL}`;
+    card.dataset = {
+        'src':images.hits[x].webformatURL
+    }
     document.querySelector(".root").appendChild(card);
 
     }
 }
 
 const fetch_images = async (values='girl') =>{
-    let url = `https://pixabay.com/api/?key=${api}&q=${encodeURIComponent(values)}&per_page=50&pretty=true`;
+    let url = `https://pixabay.com/api/?key=${api}&q=${encodeURIComponent(values)}&per_page=5&pretty=true`;
     let fetch_res = await fetch(url);
     let response_json = await fetch_res.json();
     return get_images(response_json);
