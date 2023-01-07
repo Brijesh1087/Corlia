93f84ba0c6cc268f2016e3a021f11f50d41db20c
Brijesh Kumar <98073055+Brijesh1087@users.noreply.github.com>
2023-01-07 14:58:42 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 var api = "25631434-859bf3166bb26e033af830823";
 
+const load_images = () => {
+  window.onscroll = function(ev) {
+            lazyload();
+        };
+
+    function lazyload() {
+        let img = document.querySelectorAll(".lazyimg");
+        for (let i = 0; i < img.length; i++) {
+            if (viewPort(img[i])) {
+                img[i].setAttribute("src", img[i].getAttribute("data-src"));
+            }
+        }
+    }
+
+    function viewPort(el) {
+        let rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <=
+            (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+}
 const get_images = (images) => {
     document.querySelector(".root").innerHTML = null;
-    console.log(images.hits)
-
     for(let x in images.hits){
         let card = document.createElement("img");
-    card.setAttribute('class','img-fluid mx-auto text-center rounded p-2 m-2 shadow-sm bg-white lazyimg');
-    card.src=`${images.hits[x].webformatURL}`;
-    card.dataset = {
-        'src':images.hits[x].webformatURL
+         card.setAttribute('class','img-fluid mx-auto text-center rounded p-2 m-2 shadow-sm bg-white lazyimg');
+         card.setAttribute('data-src',images.hits[x].webformatURL)
+        card.src='lazyimg.gif';
+        document.querySelector(".root").appendChild(card);
     }
-    document.querySelector(".root").appendChild(card);
-
-    }
+    load_images()
 }
 
 const fetch_images = async (values='girl') =>{
