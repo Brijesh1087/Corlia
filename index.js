8e08dfb47af3d7dbb937c5d6822a754ea686e1a7
Brijesh Kumar <98073055+Brijesh1087@users.noreply.github.com>
2023-01-07 15:57:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const load_images = () => {
 
     function viewPort(el) {
         let rect = el.getBoundingClientRect();
+        let show_top = document.querySelector("form").getBoundingClientRect().top;
+        let popup = document.querySelector(".top");
+        if(show_top >= -100){
+            popup.style.display = 'none'
+        }else {
+            popup.style.display = 'block'
+        }
         return (
             rect.top >= 0 &&
             rect.left >= 0 &&
@@ -33,13 +40,14 @@ const get_images = (images) => {
          card.setAttribute('class','img-fluid mx-auto text-center rounded p-2 m-2 shadow-sm bg-white lazyimg');
          card.setAttribute('data-src',images.hits[x].webformatURL)
         card.src='lazyimg.gif';
+        card.innerHTML = 'h1'
         document.querySelector(".root").appendChild(card);
     }
     load_images()
 }
 
 const fetch_images = async (values='girl') =>{
-    let url = `https://pixabay.com/api/?key=${api}&q=${encodeURIComponent(values)}&per_page=50&pretty=true`;
+    let url = `https://pixabay.com/api/?key=${api}&q=${encodeURIComponent(values)}&per_page=20&pretty=true`;
     let fetch_res = await fetch(url);
     let response_json = await fetch_res.json();
     return get_images(response_json);
